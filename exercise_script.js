cce1127d758455e17ee255a18840a05fa14a4533
intelligentliteracy <intelligentliteracy@gmail.com>
2016-09-12 16:16:24 -0500

    upload backups

---
+++ exercise_script.js
@@ -0,0 +1,8 @@
+
+window.alert("Welcome to my website!");
+function select_wrong(){
+    window.alert("Oops, try again!");
+}
+function select_right(){
+    window.alert("Great job, that's exactly right! ");
+}
